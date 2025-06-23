a091c4b2e03748e23719da18c86b21fd9e644714
cosmo <sleimanahs@gmail.com>
2025-06-23 15:18:09 +0300

    DF03 - added tmux plugin install

--- install.sh
+++ install.sh
@@ -65,3 +65,24 @@ case "${1:-install}" in
     echo "Usage: ./install.sh [install|uninstall]"
     ;;
 esac
+
+# installing tmux plugins
+
+TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+if [[ ! -d "$TPM_DIR" ]]; then
+  echo "Cloning TPM into $TPM_DIR..."
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+else
+  echo "TPM already installed at $TPM_DIR"
+fi
+
+chmod +x "$TPM_DIR/tpm"
+
+if pgrep -x tmux >/dev/null; then
+  echo "Reloading tmux config and installing plugins..."
+  tmux source-file ~/.tmux.conf
+  echo "Inside tmux, press prefix + I to install plugins."
+else
+  echo "Tmux is not running. Please start tmux and press prefix + I to install plugins."
+fi
